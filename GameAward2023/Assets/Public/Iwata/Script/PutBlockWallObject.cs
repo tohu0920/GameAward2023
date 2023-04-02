8fea062e9ffb428ac740cd7c7af4afc9beed3410
iwatakouki <2002gg1001@gmail.com>
2023-04-02 09:48:10 +0900

    ボタンを押してフィジックスが追加されるように変更 まだコネクトボディは設定できていない

--- GameAward2023/Assets/Public/Iwata/Script/PutBlockWallObject.cs
+++ GameAward2023/Assets/Public/Iwata/Script/PutBlockWallObject.cs
@@ -6,6 +6,7 @@ public class PutBlockWallObject : MonoBehaviour
 {
     public GameObject BlockWallObject;
     public Vector3 Size = Vector3.one;
+    public float Endure = 10;
 
     public void Export()
     {
@@ -26,6 +27,28 @@ public class PutBlockWallObject : MonoBehaviour
                     GameObject obj = Instantiate(BlockWallObject, pos, Quaternion.identity);
                     obj.transform.parent = this.transform;
                     pos.x += BlockWallObject.transform.localScale.x;
+
+                    FixedJoint[] hoge = new FixedJoint[2];
+                    hoge[0] = obj.AddComponent<FixedJoint>();
+                    hoge[1] = obj.AddComponent<FixedJoint>();
+
+                    //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    //RaycastHit hit;
+                    Ray ray = new Ray(obj.transform.position, Vector3.right);
+                    RaycastHit hit;
+                    float length = (obj.transform.localScale.x / 2) + 0.1f;
+                    Physics.Raycast(ray, out hit, length);
+                    //Debug.Log(hit.transform.name);
+                    hoge[0].connectedBody = hit.rigidbody;
+
+                    ray = new Ray(obj.transform.position, Vector3.up);
+                    //RaycastHit hit;
+                    length = (obj.transform.localScale.y / 2) + 0.1f;
+                    Physics.Raycast(ray, out hit, length);
+                    hoge[1].connectedBody = hit.rigidbody;
+
+                    hoge[0].breakForce = hoge[1].breakForce = Endure;
+                    hoge[0].breakTorque = hoge[1].breakTorque = Endure;
                 }
                 pos.x = this.transform.position.x;
                 pos.y += BlockWallObject.transform.localScale.y;
