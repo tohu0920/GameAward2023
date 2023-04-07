1ac04f6887795b2813deeeec6f9f5517c4295f6f
ikouki1001 <2002gg1001@gmail.com>
2023-04-07 11:26:25 +0900

    組み立て・組み立て後の回転・ゲームプレイ・ミニコア完成

--- GameAward2023/Assets/Public/Iwata/Script/JointJank_iwata.cs
+++ GameAward2023/Assets/Public/Iwata/Script/JointJank_iwata.cs
@@ -13,32 +13,23 @@ public class JointJank_iwata : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(this.gameObject.GetComponent<FixedJoint>())
-        {
-            Rigidbody rb = this.gameObject.GetComponent<Rigidbody>();
 
-            // オブジェクトに加えられる力とトルクを取得する
-            Vector3 force = rb.mass * rb.velocity;
-            //Vector3 torque = rb.inertiaTensor * rb.angularVelocity;
-
-            // 取得した力とトルクをログに出力する
-            Debug.Log("Force on object: " + force);
-            //Debug.Log("Torque on object: " + torque);
-
-            //Debug.Log(this.transform.position);
-        }
     }
 
     public void JointJanktoCore(Transform trans)
     {
-        FixedJoint joint = this.gameObject.AddComponent<FixedJoint>();
-        joint.connectedBody = trans.GetComponent<Rigidbody>();
-
+       
+        
+        this.transform.parent = null;
         this.transform.parent = trans.parent;
 
         Vector3 pos = trans.position;
         pos.z -= trans.localScale.z / 2.0f;
         pos.z -= this.transform.localScale.z / 2.0f;
         this.transform.position = pos;
+
+
+        FixedJoint joint = this.gameObject.AddComponent<FixedJoint>();
+        joint.connectedBody = trans.GetComponent<Rigidbody>();
     }
 }
