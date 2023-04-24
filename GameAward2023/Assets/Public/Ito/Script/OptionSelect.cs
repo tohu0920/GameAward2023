c27efa4da32c275549e02eaa459b33de3a050274
HBK <Kyo-chan.tab.116@docomo.ne.jp>
2023-04-24 15:03:07 +0900

    タイトル、オプションの更新

--- GameAward2023/Assets/Public/Ito/Script/OptionSelect.cs
+++ GameAward2023/Assets/Public/Ito/Script/OptionSelect.cs
@@ -7,74 +7,74 @@ using UnityEngine.Audio;
 public class OptionSelect : MonoBehaviour
 {
     //Sceneオブジェクトの取得
-    [SerializeReference] GameObject GameScreen;
-    [SerializeReference] GameObject OptionScreen;
-    [SerializeReference] GameObject kari;
-    [SerializeReference] AudioMixerSnapshot BGM;
+    [SerializeReference] GameObject TitleScreen;
+    [SerializeReference] GameObject OptionsScreen;
+    [SerializeReference] GameObject ControleObject;
+    [SerializeReference] AudioMixerSnapshot BGMResource;
     
     //変数宣言
-    private GameObject BGMImage1;
-    private GameObject BGMImage2;    
-    private GameObject SEImage1;
-    private GameObject SEImage2;    
-    private GameObject ReadMeImage1;
-    private GameObject ReadMeImage2;            
+    private GameObject LightBGMImage;
+    private GameObject BGMImage;    
+    private GameObject SEImage;
+    private GameObject LightSEImage;    
+    private GameObject ReadMeImage;
+    private GameObject LightReadMeImage;            
 
-    public Slider bgmSlider;         // Sliderを格納する変数
-    public Slider SESlider;          // SEを格納する変数
-    public AudioSource BGMSource;    // BGMを再生するAudioSourceを格納する変数
-    public AudioSource SESourse;     // SE再生するAudioSourceを格納する変数
+    public Slider bgmVolumeSlider;         // Sliderを格納する変数
+    public Slider SEVolumeSlider;          // SEを格納する変数
+    public AudioSource BGMSource_camera;    // BGMを再生するAudioSourceを格納する変数
+    public AudioSource SESourse_camera;     // SE再生するAudioSourceを格納する変数
 
-    public int SelectOptionNum; //UI選択用
+    public int OptionNum; //UI選択用
 
     // Start is called before the first frame update
     void Start()
     {
         //Imageの取得
-        BGMImage1 = GameObject.Find("BGMImage1");       //光っている
-        BGMImage2 = GameObject.Find("BGMImage2");       //光っていない
-        SEImage1 = GameObject.Find("SEImage1");         //光っていない
-        SEImage2 = GameObject.Find("SEImage2");         //光っている
-        ReadMeImage1 = GameObject.Find("ReadMeImage1"); //光っていない
-        ReadMeImage2 = GameObject.Find("ReadMeImage2"); //光っている
+        LightBGMImage = GameObject.Find("BGMImage1");       //光っている
+        BGMImage      = GameObject.Find("BGMImage2");       //光っていない
+        SEImage       = GameObject.Find("SEImage1");         //光っていない
+        LightSEImage  = GameObject.Find("SEImage2");         //光っている
+        ReadMeImage   = GameObject.Find("ReadMeImage1"); //光っていない
+        LightReadMeImage = GameObject.Find("ReadMeImage2"); //光っている
 
         //BGM・SEの初期音量設定
-        bgmSlider.value = 5.0f;
-        SESlider.value = 5.0f;
+        bgmVolumeSlider.value = 5.0f;
+        SEVolumeSlider.value  = 5.0f;
 
         //選択用変数の初期化
-        SelectOptionNum = 0;
+        OptionNum = 0;
     }
 
     // Update is called once per frame
     void Update()
     {    
         //選択変更処理
-        SelectOptionNum -= AxisInput.GetAxisRawRepeat("Vertical_PadX");
+        OptionNum -= AxisInput.GetAxisRawRepeat("Vertical_PadX");
 
         //選択のループ
-        SelectOptionNum += 3;
-        SelectOptionNum %= 3;
+        OptionNum += 3;
+        OptionNum %= 3;
         
-        switch (SelectOptionNum)
+        switch (OptionNum)
         {
             case 0:
                 //音量がひかっている
-                ChangeActive();
+                ChangeImageActive();
 
-                bgmSlider.value += AxisInput.GetAxisRawRepeat("Horizontal_PadX") * 5.0f;
+                bgmVolumeSlider.value += AxisInput.GetAxisRawRepeat("Horizontal_PadX") * 5.0f;
                 break;
 
             case 1:
                 //効果音が光っている
-                ChangeActive();
+                ChangeImageActive();
 
-                SESlider.value += AxisInput.GetAxisRawRepeat("Horizontal_PadX") * 5.0f;
+                SEVolumeSlider.value += AxisInput.GetAxisRawRepeat("Horizontal_PadX") * 5.0f;
                 break;
 
             case 2:
                 //操作説明が光っている
-                ChangeActive();
+                ChangeImageActive();
 
                 //if (PadInput.GetKeyDown(KeyCode.JoystickButton0))
 
@@ -83,9 +83,9 @@ public class OptionSelect : MonoBehaviour
 
         if (PadInput.GetKeyDown(KeyCode.JoystickButton1))
         {
-            GameScreen.SetActive(true);
-            OptionScreen.SetActive(false);
-            kari.SetActive(true);
+            TitleScreen.SetActive(true);
+            OptionsScreen.SetActive(false);
+            ControleObject.SetActive(true);
         }
     }
 
@@ -95,7 +95,7 @@ public class OptionSelect : MonoBehaviour
     /// <param name="volume"></param>
     public void SetBGM(float volume)
     {
-        BGM.audioMixer.SetFloat("BGM", volume);
+        BGMResource.audioMixer.SetFloat("BGM", volume);
         Debug.Log(volume);
     }
 
@@ -105,41 +105,41 @@ public class OptionSelect : MonoBehaviour
     /// <param name="volume"></param>
     public void SetSE(float volume)
     {
-        BGM.audioMixer.SetFloat("SE", volume);
+        BGMResource.audioMixer.SetFloat("SE", volume);
     }
 
     /// <summary>
     /// /// イメージアクティブ・非アクティブ変更関数
     /// </summary>
-    public void ChangeActive()
+    public void ChangeImageActive()
     {
-        switch(SelectOptionNum)
+        switch(OptionNum)
         {
             case 0:
-                BGMImage1.SetActive(true);
-                BGMImage2.SetActive(false);
-                SEImage1.SetActive(true);
-                SEImage2.SetActive(false);
-                ReadMeImage1.SetActive(true);
-                ReadMeImage2.SetActive(false);
+                LightBGMImage.SetActive(true);
+                BGMImage.SetActive(false);
+                SEImage.SetActive(true);
+                LightSEImage.SetActive(false);
+                ReadMeImage.SetActive(true);
+                LightReadMeImage.SetActive(false);
                 break;
 
             case 1:
-                BGMImage1.SetActive(false);
-                BGMImage2.SetActive(true);
-                SEImage1.SetActive(false);
-                SEImage2.SetActive(true);
-                ReadMeImage1.SetActive(true);
-                ReadMeImage2.SetActive(false);
+                LightBGMImage.SetActive(false);
+                BGMImage.SetActive(true);
+                SEImage.SetActive(false);
+                LightSEImage.SetActive(true);
+                ReadMeImage.SetActive(true);
+                LightReadMeImage.SetActive(false);
                 break;
 
             case 2:
-                BGMImage1.SetActive(false);
-                BGMImage2.SetActive(true);
-                SEImage1.SetActive(true);
-                SEImage2.SetActive(false);
-                ReadMeImage1.SetActive(false);
-                ReadMeImage2.SetActive(true);
+                LightBGMImage.SetActive(false);
+                BGMImage.SetActive(true);
+                SEImage.SetActive(true);
+                LightSEImage.SetActive(false);
+                ReadMeImage.SetActive(false);
+                LightReadMeImage.SetActive(true);
                 break;
         }
     }
