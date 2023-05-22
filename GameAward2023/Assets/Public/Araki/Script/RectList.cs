08fee95f76d4926fef5805066bdf52be69c2d52e
WNHS10494\kenta <kentarou20020726@gmail.com>
2023-05-22 15:28:11 +0900

    画像切り替え用関数追加

--- GameAward2023/Assets/Public/Araki/Script/RectList.cs
+++ GameAward2023/Assets/Public/Araki/Script/RectList.cs
@@ -1,24 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RectList : MonoBehaviour
 {
-	List<RectTransform> m_rectList;
+	List<GameObject> m_rectList;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-		m_rectList = new List<RectTransform>();
+		m_rectList = new List<GameObject>();
 
 		//--- 子のRectTransformをリスト化
 		int childCnt = transform.childCount;
 		for (int i = 0; i < childCnt; i++)
 		{
-			RectTransform rectTransform = transform.GetChild(i).GetComponent<RectTransform>();
-
-			if (rectTransform == null) continue;
-			m_rectList.Add(rectTransform);
+			m_rectList.Add(transform.GetChild(i).gameObject);
 		}
 	}
 
@@ -28,8 +26,35 @@ public class RectList : MonoBehaviour
 
 	}
 
+	/// <summary>
+	/// 画像の切り替え
+	/// </summary>
+	/// <param name="index">区画番号</param>
+	/// <param name="image">画像データ</param>
+	void ChangeImage(int index, Sprite image)
+	{
+		if (index >= m_rectList.Count) return;
+		Image currentImage = m_rectList[index].GetComponent<Image>();
+		if (currentImage == null) return;
+		currentImage.sprite = image;
+	}
+
 	List<RectTransform> rectList
 	{
-		get { return m_rectList; }
+		get {
+			List<RectTransform>list = new List<RectTransform>();
+
+			//--- 子のRectTransformをリスト化
+			int childCnt = transform.childCount;
+			for (int i = 0; i < childCnt; i++)
+			{
+				RectTransform rect = transform.GetChild(i).GetComponent<RectTransform>();
+				if (rect == null) continue;
+
+				list.Add(rect);
+			}
+
+			return list;
+		}
 	}
 }
