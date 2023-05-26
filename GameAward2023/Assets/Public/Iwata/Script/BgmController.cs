e82794cb07f92b1918fa399e96dbac42f92bea2e
iwatakouki <2002gg1001@gmail.com>
2023-05-27 08:47:22 +0900

    Optionの音量調節適応

--- GameAward2023/Assets/Public/Iwata/Script/BgmController.cs
+++ GameAward2023/Assets/Public/Iwata/Script/BgmController.cs
@@ -5,10 +5,46 @@ using UnityEngine.SceneManagement;
 
 public class BgmController : MonoBehaviour
 {
+    public BgmController instance;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     [SerializeField] AudioManager.BGMKind bgm;
 
     private void Start()
     {
         AudioManager.PlayBGM(bgm);
+    }
+
+    private void OnEnable()
+    {
+        // シーン遷移時のコールバックを登録
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        // シーン遷移時のコールバックを解除
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        // シーン遷移時の処理をここに記述
+        if (scene.name == "")
+        {
+            // 特定のオブジェクトを持ち越す処理など
+        }
     }
 }
